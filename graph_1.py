0f9ce1dc664a540d0e2034482e61a207446ee90a
Nyan Lin <nyanlin@Nyans-MacBook-Pro.local>
2022-05-29 08:04:15 +0630

    lesson for elementry graph alogirth

---
+++ graph_1.py
@@ -0,0 +1,21 @@
+class Node(object):
+    def __init__(self, name):
+        self.name = name
+    def getName(self):
+        return self.name
+    def __str__(self):
+        return self.name
+
+class Edge(object):
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+
+    def getSrc(self):
+        return self.src
+    
+    def getDest(self):
+        return self.dest
+    
+    def __str__(self):
+        return self.src.getSrc()+'---->'+self.dest.getDest()
