10f3f14b101341ac8f4dcfeadd7aae9cb6b4a17b
Nyan Lin <nyanlin@Nyans-MacBook-Pro.local>
2022-05-31 08:07:21 +0630

    add city graph

--- graph_1.py
+++ graph_1.py
@@ -1,7 +1,4 @@
-from math import dist
-
-
-class Node(object):
+class node(object):
     def __init__(self, name):
         self.name = name
     def getName(self):
@@ -9,7 +6,7 @@ class Node(object):
     def __str__(self):
         return self.name
 
-class Edge(object):
+class edge(object):
     def __init__(self, src, dest):
         self.src = src
         self.dest = dest
@@ -21,14 +18,14 @@ class Edge(object):
         return self.dest
     
     def __str__(self):
-        return self.src.getSrc()+'---->'+self.dest.getDest()
+        return self.getSrc()+' ----> '+ self.getDest()
 
 class graph(object):
     def __init__(self):
         self.edges = {}
     
     def addNode(self, node):
-        if node in self.edge:
+        if node in self.edges:
             raise ValueError("Duplicated Node")
         else:
             self.edges[node]=[]
@@ -41,6 +38,13 @@ class graph(object):
         else:
             self.edges[src].append(dest)
 
+    def getNode(self,name):
+        for n in self.edges:
+            if n == name:
+                return n
+            else:
+                NameError(name)
+
     def getChildren(self, node):
         return self.edges[node]
 
@@ -51,12 +55,24 @@ class graph(object):
         result = ''
         for src in self.edges:
             for dest in self.edges[src]:
-                result = result + src.getName() + '----->'+dest.getName()
+                result = result + src + '----->'+dest+'\n'
         return result[:-1]
-
+        
 class Graph(graph):
     def addEdge(self, edge):
         graph.addEdge(self, edge)
         rev = Edge(edge.getDestination(), edge.getSource())
         graph.addEdge(self, rev)
 
+
+def cityGraph(graphType):
+    for node in ('Maw','Than','Mu','Yan','Sag','Man'):
+        graphType.addNode(node)
+    graphType.addEdge(edge(graphType.getNode('Maw'), graphType.getNode('Yan')))
+    graphType.addEdge(edge(graphType.getNode('Than'), graphType.getNode('Sag')))
+    graphType.addEdge(edge(graphType.getNode('Man'), graphType.getNode('Maw')))
+    graphType.addEdge(edge(graphType.getNode('Mu'), graphType.getNode('Sag')))
+    graphType.addEdge(edge(graphType.getNode('Maw'), graphType.getNode('Yan')))
+    return graphType
+
+print(cityGraph(graph()))
