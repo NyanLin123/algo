0bc55f6bd638721de83de95e902f7281edc422a0
Nyan Lin <nyanlin@Nyans-MacBook-Pro.local>
2022-06-08 07:31:57 +0630

    add bell man

--- graph_1.py
+++ graph_1.py
@@ -130,3 +130,48 @@ print('------------')
 print("Breath First Search")
 bfs(g,'maw','pyin', toPrint=True)
 
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+        self.nodes = []
+
+    def add_edge(self, s, d, w):
+        self.graph.append([s, d, w])
+
+    def addNode(self, value):
+        self.nodes.append(value)
+
+    def print_solution(self, dist):
+        print("Distance of vertex from source")
+        for key, value in dist.items():
+            print(' '+key, ' :   ', value)
+
+    def bellmanFord(self, src):
+        dist = {i: float("Inf") for i in self.nodes}
+        dist[src] = 0
+
+        for temp in range(self.V-1):
+            for s, d, w in self.graph:
+                if dist[s] != float("Inf") and dist[s] + w < dist[d]:
+                    dist[d] = dist[s] + w
+
+            for s, d, w in self.graph:
+                if dist[s] != float("Inf") and dist[s] + w < dist[d]:
+                    print("Graph contains negative cycle")
+
+            return self.print_solution(dist)
+
+
+g = Graph(5)
+lst = ["A", "B", "C", "D", "E"]
+for i in lst:
+    g.addNode(i)
+
+g.add_edge("A", "C", 6)
+g.add_edge("A", "D", 5)
+g.add_edge("B", "C", 1)
+g.add_edge("A", "B", 2)
+g.add_edge("C", "E", 2)
+
+g.bellmanFord("E")
