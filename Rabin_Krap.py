f7de2e13a04492f90411687a2af02c3336422275
Nyan Lin <nyanlin@Nyans-MacBook-Pro.local>
2022-06-26 22:13:00 +0630

    adding rabin file

---
+++ Rabin_Krap.py
@@ -0,0 +1,13 @@
+
+pattern = 'apreakappleseedonthetable'
+text= 'apple'
+identifire = {'a':1,'b':2,'c':3}
+
+def Rabin_Krap(pattern, text):
+
+    pattern_list = list(pattern)
+    text_list = list(text)
+
+    return "success"
+
+print(Rabin_Krap('a','b'))
