6973b3480f5fa7ce38786917bb3c3ce86804db1f
Nyan Lin <nyanlin@Nyans-MacBook-Pro.local>
2022-06-28 12:45:53 +0630

    adding nav

--- ch32/navie.py
+++ ch32/navie.py
@@ -1,14 +1,19 @@
 pat = 'abcdacedeacd'
-txt = 'abc'
+txt = 'abcda'
 
 def nav(pattern, text):
     abs_pat = len(pattern)
     abs_txt = len(text)
 
     for i in range(abs_pat-abs_txt+1):
-        # for j in range(abs_txt):
-            
-        print(pattern[i])
+        j = 0
+        while(j<abs_txt):
+            if pattern[i+j] == text[j]:
+                j += 1
+            else:
+                break
+        else:
+            print('fount at position end at ', j)
 
 
 nav(pat, txt)
