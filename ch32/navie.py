6b1cb482e5b7aa87444b2c2466d54ded5a0c7caa
Nyan Lin <nyanlin@Nyans-MacBook-Pro.local>
2022-06-28 08:43:11 +0630

    continue

--- ch32/navie.py
+++ ch32/navie.py
@@ -1,2 +1,14 @@
-def inter(request):
-    return "sumiting"
+pat = 'abcdacedeacd'
+txt = 'abc'
+
+def nav(pattern, text):
+    abs_pat = len(pattern)
+    abs_txt = len(text)
+
+    for i in range(abs_pat-abs_txt+1):
+        # for j in range(abs_txt):
+            
+        print(pattern[i])
+
+
+nav(pat, txt)
